4a6a1aab82fe884e3cf8e0b7dbc49c6c6fc534d4
Ben Armstrong <BArmstrong@dymaxion.ca>
2006-10-19 17:25:10 +0000

    Pre-IPv6 compatibility patch (UCX < 5.3)

--- ruby/vms/vms.h
+++ ruby/vms/vms.h
@@ -12,4 +12,12 @@ extern int flock(int fd, int oper);
 
 extern int vsnprintf();
 
+/* For compatibility with pre-IPv6 TCP/IP stack on VMS (UCX < 5.3) */
+#define sockaddr_storage sockaddr
+#define HAVE_SOCKADDR_STORAGE 1
+
+#ifdef INET6
+#undef INET6
+#endif
+
 #endif
