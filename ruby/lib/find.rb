8ad5622c00a22a847463fcf14fbdc0904bc48c4a
Ben Armstrong <BArmstrong@dymaxion.ca>
2006-10-19 17:38:37 +0000

    Support VMS file paths

--- ruby/lib/find.rb
+++ ruby/lib/find.rb
@@ -1,79 +1,59 @@
-#
-# find.rb: the Find module for processing all files under a given directory.
-#
-
-#
-# The +Find+ module supports the top-down traversal of a set of file paths.
-#
-# For example, to total the size of all files under your home directory,
-# ignoring anything in a "dot" directory (e.g. $HOME/.ssh):
-#
-#   require 'find'
-#
-#   total_size = 0
-#
-#   Find.find(ENV["HOME"]) do |path|
-#     if FileTest.directory?(path)
-#       if File.basename(path)[0] == ?.
-#         Find.prune       # Don't look any further into this directory.
-#       else
-#         next
-#       end
-#     else
-#       total_size += FileTest.size(path)
-#     end
-#   end
-#
 module Find
-
-  #
-  # Calls the associated block with the name of every file and directory listed
-  # as arguments, then recursively on their subdirectories, and so on.
-  #
-  # See the +Find+ module documentation for an example.
-  #
-  def find(*paths) # :yield: path
-    paths.collect!{|d| d.dup}
-    while file = paths.shift
-      catch(:prune) do
-        next unless File.exist? file
-	yield file.dup.taint
-	begin
-	  if File.lstat(file).directory? then
-	    d = Dir.open(file)
-	    begin
-	      for f in d
-		next if f == "." or f == ".."
-		if File::ALT_SEPARATOR and file =~ /^(?:[\/\\]|[A-Za-z]:[\/\\]?)$/ then
-		  f = file + f
-		elsif file == "/" then
-		  f = "/" + f
-		else
-		  f = File.join(file, f)
-		end
-		paths.unshift f.untaint
-	      end
-	    ensure
-	      d.close
-	    end
-	  end
-       rescue Errno::ENOENT, Errno::EACCES
-	end
-      end
+    # Calls the associated block with the name of every file and directory listed
+    # as arguments, then recursively on their subdirectories, and so on.
+    #
+    # See the +Find+ module documentation for an example.
+    def find *paths  # :yield: path
+        paths.collect!{|path| path.dup} # Why?
+        while path=paths.shift
+            catch :prune do
+                yield path.dup.taint
+                next unless File.exist? path
+                begin
+                    if File.lstat(path).directory?
+                        dir=Dir.open path
+                        begin
+                            for file in dir
+                                if path=~/(^.*?)\](.*?)\.DIR;\d+/i
+                                    file=$1+'.'+$2+']'+file
+                                else
+                                    file=path+file
+                                end
+                                paths.unshift file.untaint
+                            end
+                        ensure
+                            dir.close
+                        end
+                    end
+                    rescue Errno::ENOENT, Errno::EACCES
+                end
+            end
+        end
     end
-  end
 
-  #
-  # Skips the current file or directory, restarting the loop with the next
-  # entry. If the current file is a directory, that directory will not be
-  # recursively entered. Meaningful only within the block associated with
-  # Find::find.
-  #
-  # See the +Find+ module documentation for an example.
-  #
-  def prune
-    throw :prune
-  end
+    # Skips the current file or directory, restarting the loop with the next
+    # entry. If the current file is a directory, that directory will not be
+    # recursively entered. Meaningful only within the block associated with
+    # Find::find.
+    #
+    # See the +Find+ module documentation for an example.
+    def prune
+        throw :prune
+    end
 
-  module_function :find, :prune
+    module_function :find, :prune
+end
+
+if $0==__FILE__
+
+    Find.find '[DYMAX.PRO.RUBY]' do |file|
+        type=case
+            when File.file?(file): "F"
+            when File.directory?(file): "D"
+            else "?"
+        end
+        puts "#{type}: #{file}"
+        Find.prune if file =~ /OBS/i
+    end
+
 end
