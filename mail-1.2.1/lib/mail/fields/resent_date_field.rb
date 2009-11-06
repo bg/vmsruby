f61bae874d3e712ff07b38d97b30642aa153c6e1
Ben Armstrong <BArmstrong@dymaxion.ca>
2009-11-06 14:05:41 +0000

    Import Mail library

---
+++ mail-1.2.1/lib/mail/fields/resent_date_field.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+# 
+# resent-date     =       "Resent-Date:" date-time CRLF
+module Mail
+  class ResentDateField < StructuredField
+    
+    include Mail::CommonDate
+    
+    FIELD_NAME = 'resent-date'
+    CAPITALIZED_FIELD = 'Resent-Date'
+    
+    def initialize(*args)
+      super(CAPITALIZED_FIELD, strip_field(FIELD_NAME, args.last))
+    end
+    
+    def encoded
+      do_encode(CAPITALIZED_FIELD)
+    end
+    
+    def decoded
+      do_decode
+    end
+    
+  end
+end
